7e6095b2cbf57bc15ddbf496bdde71d1f6e8a63e
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-10-21 10:49:11 +0200

    resolved for master

--- components/lib/agents.tsx
+++ components/lib/agents.tsx
@@ -1,47 +1,50 @@
 export const agents = [
-   
-    {
-        id: 1,
-        name: 'Tim Hortons',
-        description:"Tim is a real estate agent at Kaleidico Realty, with over 8 years of experience. Lynn prides himself in satisfying his clients with the real sgsg sfgsgshhsffhsrwywrgsf",
-        image: "/images/agent.jpg",
-        rating: 5
-    },
-    {
-        id: 1,
-        name: 'Jimmy Banks',
-        description:"Tim is a real estate agent at Kaleidico Realty, with over 8 years of experience. Lynn prides himself in satisfying his clients with the real sgsg sfgsgshhsffhsrwywrgsf",
-        image: "/images/agent.jpg",
-        rating: 3
-    },
-    {
-        id: 1,
-        name: '2pac Brainz',
-        description:"Tim is a real estate agent at Kaleidico Realty, with over 8 years of experience. Lynn prides himself in satisfying his clients with the real sgsg sfgsgshhsffhsrwywrgsf",
-        image: "/images/agent.jpg",
-        rating: 4
-    },
-    {
-        id: 1,
-        name: 'John Doe',
-        description:"Tim is a real estate agent at Kaleidico Realty, with over 8 years of experience. Lynn prides himself in satisfying his clients with the real sgsg sfgsgshhsffhsrwywrgsf",
-        image: "/images/agent.jpg",
-        rating: 5
-    },
-    {
-        id: 1,
-        name: 'Durk Banks',
-        description:"Tim is a real estate agent at Kaleidico Realty, with over 8 years of experience. Lynn prides himself in satisfying his clients with the real sgsg sfgsgshhsffhsrwywrgsf",
-        image: "/images/agent.jpg",
-        rating: 3
-    },
-    {
-        id: 1,
-        name: 'Stephanie Waters',
-        description:"Tim is a real estate agent at Kaleidico Realty, with over 8 years of experience. Lynn prides himself in satisfying his clients with the real sgsg sfgsgshhsffhsrwywrgsf",
-        image: "/images/agent.jpg",
-        rating: 4
-    },
-   
-  
-]
+  {
+    id: 1,
+    name: "Tim Hortons",
+    description:
+      "Tim is a real estate agent at Kaleidico Realty, with over 8 years of experience. Lynn prides himself in satisfying his clients with the real sgsg sfgsgshhsffhsrwywrgsf",
+    image: "/images/agent.jpg",
+    rating: 5,
+  },
+  {
+    id: 1,
+    name: "Jimmy Banks",
+    description:
+      "Tim is a real estate agent at Kaleidico Realty, with over 8 years of experience. Lynn prides himself in satisfying his clients with the real sgsg sfgsgshhsffhsrwywrgsf",
+    image: "/images/agent.jpg",
+    rating: 3,
+  },
+  {
+    id: 1,
+    name: "2pac Brainz",
+    description:
+      "Tim is a real estate agent at Kaleidico Realty, with over 8 years of experience. Lynn prides himself in satisfying his clients with the real sgsg sfgsgshhsffhsrwywrgsf",
+    image: "/images/agent.jpg",
+    rating: 4,
+  },
+  {
+    id: 1,
+    name: "John Doe",
+    description:
+      "Tim is a real estate agent at Kaleidico Realty, with over 8 years of experience. Lynn prides himself in satisfying his clients with the real sgsg sfgsgshhsffhsrwywrgsf",
+    image: "/images/agent.jpg",
+    rating: 5,
+  },
+  {
+    id: 1,
+    name: "Durk Banks",
+    description:
+      "Tim is a real estate agent at Kaleidico Realty, with over 8 years of experience. Lynn prides himself in satisfying his clients with the real sgsg sfgsgshhsffhsrwywrgsf",
+    image: "/images/agent.jpg",
+    rating: 3,
+  },
+  {
+    id: 1,
+    name: "Stephanie Waters",
+    description:
+      "Tim is a real estate agent at Kaleidico Realty, with over 8 years of experience. Lynn prides himself in satisfying his clients with the real sgsg sfgsgshhsffhsrwywrgsf",
+    image: "/images/agent.jpg",
+    rating: 4,
+  },
+];
